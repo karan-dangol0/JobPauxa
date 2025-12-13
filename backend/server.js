d3f9f6474843790fa7180734367f4502ec929d33
Karan Dangol <150545082+karan-dangol0@users.noreply.github.com>
2025-12-13 19:17:38 +0545

    some feature

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,7 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import userRouter from "./route/user.route.js";
-
 
 const app = express();
 app.use(cors());
@@ -14,6 +12,5 @@ app.use("/api/v1", userRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT || 3000, () => {
-    console.log(`Server is started on port ${PORT || 3000}`);
-    
-})
+  console.log(`Server is started on port ${PORT || 3000}`);
+});
